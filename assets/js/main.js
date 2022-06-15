cbce79818c0e3d00fb77352cf3855414edc5635e
ensar2318 <ensar163029@gmail.com>
2022-06-15 12:57:43 +0300

    asd

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    // Hero Swiper kod kısmı
     var heroSwiper = new Swiper(".heroSwiper", {
         fadeEffect: { crossFade: true },
         virtualTranslate: true,
@@ -14,4 +15,86 @@ $(document).ready(function() {
         slidersPerView: 1,
         effect: "fade"
     });
+    // Hero Swiper kod kısmı
+
+
+
+    // What We Do Swiper filtreleme ve kaydırma kısmı
+    var whatWeDoSwiper = new Swiper(".whatWeDoSwiper", {
+        speed: 1000,
+        slidersPerView: 1,
+        spaceBetween: 200,
+        navigation: {
+            nextEl: ".what-btn-next",
+            prevEl: ".what-btn-prev",
+        }
+    });
+    $("#whatWeDo ol li").click(function(e) {
+        e.preventDefault();
+        $("#whatWeDo ol li").removeClass("active");
+        $(this).addClass("active");
+        let filteredValue = $(this).attr("filter");
+
+
+        $(".whatWeDoSwiper .swiper-slide").each(function(index, element) {
+            // element == this
+            if (!$(element).hasClass(filteredValue)) {
+                $(element).addClass("remove");
+            } else {
+                $(element).removeClass("remove");
+            }
+        });
+        whatWeDoSwiper.destroy();
+        whatWeDoSwiper = new Swiper(".whatWeDoSwiper", {
+            speed: 1000,
+            slidersPerView: 1,
+            spaceBetween: 200,
+            navigation: {
+                nextEl: ".what-btn-next",
+                prevEl: ".what-btn-prev",
+            }
+        });
+
+    });
+
+    // What We Do Swiper filtreleme ve kaydırma kısmı
+
+
+    //What else we do slider kısmı
+    var whatElseWeDoSwiper = new Swiper(".whatElseWeDoSwiper", {
+        slidesPerView: 1,
+        speed: 1000,
+        spaceBetween: 20,
+        loop: true,
+        navigation: {
+            nextEl: ".what-else-btn-next",
+            prevEl: ".what-else-btn-prev",
+        },
+        autoplay: {
+            delay: 2000,
+        },
+        breakpoints: {
+
+            499: {
+                slidesPerView: 2,
+                spaceBetweenSlides: 30
+            },
+            1000: {
+                slidesPerView: 3,
+                spaceBetweenSlides: 40
+            },
+
+            1200: {
+                slidesPerView: 4,
+                spaceBetweenSlides: 40
+            },
+            1400: {
+                slidesPerView: 5,
+                spaceBetweenSlides: 40
+            }
+        }
+    });
+    //What else we do slider kısmı
+
+
 });
